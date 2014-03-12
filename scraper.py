ed94e431fc0b703d40826c82a536dbadbc4d1533
Steven Speicher <stevenspeicher@gmail.com>
2014-03-12 10:00:50 -0500

    Update scraper and data

--- scraper.py
+++ scraper.py
@@ -3,23 +3,15 @@ import urllib2
 import StringIO
 import os
 
-# site = '06674500' 	# State Line Gauge
-# site = '06764880'  	# Roscoe
-# site = '06775500'  	# Dunning
-# site = '06785000'  	# Saint Paul
-# site = '06768000'  	# Overton
-# site = '06770200'  	# Kearney
-# site = '06770500'  	# Grand Island
-# site = '06796000'  	# North Bend
-# site = '06805500'  	# Louisville
-
 s = ['06674500','06764880','06775500','06785000','06768000','06770200','06770500','06796000','06805500']
 s_names = ['state-line-gauge','roscoe','dunning','saint-paul','overton','kearney','grand-island','north-bend','louisville']
 
+b_d = '2011'		# Begin date - head of the year
+e_d = '2013'		# End date - tail of the year
 
 for i in range(0,len(s)):
 	# Open webpage and read data
-	url = 'http://waterdata.usgs.gov/ne/nwis/dv?cb_00060=on&format=rdb&period=&begin_date=2013-01-01&end_date=2013-12-31&site_no=' + s[i] + '&referred_module=sw'
+	url = 'http://waterdata.usgs.gov/ne/nwis/dv?cb_00060=on&format=rdb&period=&begin_date=' + b_d + '-01-01&end_date=' + e_d + '-12-31&site_no=' + s[i] + '&referred_module=sw'
 	page = urllib2.urlopen(url)
 	reader = csv.reader(page, dialect='excel-tab')
 
