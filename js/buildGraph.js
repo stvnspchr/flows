e84074a931db1d48a25cbed6b86711f0a8e1cb6c
Steven Speicher <stevenspeicher@gmail.com>
2014-04-03 15:59:48 -0500

    Making some updates

--- js/buildGraph.js
+++ js/buildGraph.js
@@ -8,9 +8,12 @@ function buildGraph(fileName) {
 	keyColor = function(d, i) {return colors(d.key)};
 
 	var myColors = ['rgb(215,48,39)','rgb(244,109,67)','rgb(253,174,97)','rgb(254,224,144)','rgb(240,240,191)','rgb(224,230,202)','rgb(171,217,233)','rgb(116,173,209)','rgb(69,117,180)'];
-
+    var myColors = ['rgb(84,48,5)','rgb(140,81,10)','rgb(191,129,45)','rgb(223,194,125)','rgb(246,232,195)','rgb(199,234,229)','rgb(128,205,193)','rgb(53,151,143)','rgb(1,102,94)','rgb(0,60,48)'];
+    
  	d3.json('data/' + fileName + '.json', function(error, data) {
-		nv.addGraph(function() {
+		nv.addGraph(function(){
+            
+            
 			chart = nv.models.stackedAreaChart()
 					.x(function(d) { return d[0] })
 					.y(function(d) { return d[1] })
@@ -24,7 +27,9 @@ function buildGraph(fileName) {
 
 			chart.xAxis
 				.showMaxMin(false)
-				.tickFormat(function(d) { return d3.time.format('%m/%d/%y')(new Date(d)) });
+				.tickFormat(function(d) { return d3.time.format('%m/%d')(new Date(d)) });
+            
+            chart.xScale(d3.time.scale());
 
 			chart.yAxis
 				.showMaxMin(true)
